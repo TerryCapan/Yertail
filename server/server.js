dff79710a84ef6f28707fc171e016e80a8909fe6
Terry Capan <tcapanjr@gmail.com>
2016-04-04 14:31:02 -0500

    add server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Recipe = require('./recipeModel.js');
+var path = require('path');
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static(__dirname + '/../../client'));
+
+
+app.get('*', function(req, res) {
+  res.sendfile(path.resolve('client/index.html'));
+})
+
+var port = process.env.PORT || 4568;
+
+app.listen(port);
+
+console.log('Server now listening on port ' + port);
+
+module.exports = app;
